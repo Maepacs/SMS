780a468cbab58545195e02a198a9096cb9038006
smile2003 <ismaelsaripada123@gmail.com>
2024-10-02 17:00:59 +0800

    logout

--- logout.php
+++ logout.php
@@ -1,6 +1,38 @@
 <?php
+// Start session
 session_start();
-session_destroy();
-header('Location: admin_login.php'); // redirect to login page
-exit();
+
+if (isset($_SESSION['username'])) {
+    // Confirm logout
+    if (isset($_GET['confirm']) && $_GET['confirm'] == 'true') {
+        // Unset all of the session variables
+        $_SESSION = array();
+
+        // Destroy the session
+        session_destroy();
+
+        // Redirect to login page
+        header("Location: login.php");
+        exit;
+    } else {
+        // Ask for confirmation using JavaScript
+        $referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'admin_dashboard.php';
+        ?>
+        <script>
+        // JavaScript confirmation dialog
+        if (confirm("Are you sure you want to log out?")) {
+            // If user confirms, proceed to logout.php?confirm=true
+            window.location.href = "logout.php?confirm=true";
+        } else {
+            // If user cancels, redirect to the previous page
+            window.location.href = "<?php echo $referrer; ?>";
+        }
+        </script>
+        <?php
+    }
+} else {
+    // If user is not logged in, redirect to login page
+    header("Location: login.php");
+    exit;
+}
 ?>
